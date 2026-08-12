3d50ad059493c01ec6a6cffbbcfa546e20bd2dcb
Sachi Jain <sachijain2026@gmail.com>
2026-08-12 09:26:18 +0530

    DSA upload

--- Stacks.cpp
+++ Stacks.cpp
@@ -1,14 +1,72 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
 using namespace std;
-int main(){
-    stack<int>s;
-    s.push(2);
-    s.push(4);
-    s.push(6);
-    s.push(8);
-    s.push(10);
-    cout<<s.top()<<endl;
+class Stack
+{
+public:
+    int *arr;
+    int top;
+    int size;
+    Stack(int s)
+    {
+        this->size = s;
+        arr = new int[size]; // dynamic allocation
+        top = -1;
+    }
+    void push(int val)
+    {
+        if ((size - top) >= 1)
+        {
+            top++;
+            arr[top] = val;
+            
+        }
+        else
+        {
+            cout << "Stack OverFlow" << endl;
+        }
+    }
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "Stack Underflow" << endl;
+        }
+        else
+        {
+            top--;
+        }
+    }
+    int peek()
+    {
+        if (top >= 0)
+        {
+            cout << arr[top] << endl;
+        }
+        else
+        {
+            cout << "Stack UnderFlow" << endl;
+        }
+    }
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
+int main()
+{
+    Stack s1(5);
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    s1.push(4);
+    s1.peek();
 
-    return 0;
+    
 }
