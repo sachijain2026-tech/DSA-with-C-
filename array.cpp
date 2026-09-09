aeaf49f2dd619593f4aa2ffb5ddb830725ab910b
Sachi Jain <sachijain2026@gmail.com>
2026-09-09 23:21:30 +0530

    DSA

--- array.cpp
+++ array.cpp
@@ -519,10 +519,45 @@ int main()
 }
     */
 
-//Majority Element
+    /*
+// Majority Element
 #include <iostream>
 using namespace std;
 #include <vector>
-int majorityElement(){
-    
+int majorityElement(vector<int> &vec)
+{
+    int count = 0;
+    int elt;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (count == 0){
+            count = 1;
+            elt = vec[i];
+        }
+        else if (elt = vec[i]){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+    return elt;
 }
+int main()
+{
+    vector<int> vec;
+    int n;
+    cout << "How many elts u wanna enter?: ";
+    cin >> n;
+    cout << "Enter the elements:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int elt;
+        cin >> elt;
+        vec.push_back(elt);
+    }
+    cout << "Majority element is: " << majorityElement(vec);
+}
+    */
+
+//Rotate Array
